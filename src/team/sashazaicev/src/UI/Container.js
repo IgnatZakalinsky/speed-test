0a5453824f6b858ddf6b2d5db3fa1d2e3a3d188d
SashaZaicev <sasha167@bk.ru>
2019-10-19 17:32:43 +0300

    i'm first

---
+++ src/team/sashazaicev/src/UI/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PComponents from "./PComponent";
+
+const Container = () => {
+    return (
+        <div>
+            <PComponents/>
+        </div>
+    )
+
+}
+export default Container
