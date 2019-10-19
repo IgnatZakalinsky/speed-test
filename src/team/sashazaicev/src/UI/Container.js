d86193f13e0efbfa282d53a9009fc353e15bdbee
SashaZaicev <sasha167@bk.ru>
2019-10-19 18:46:00 +0300

    add input

--- src/team/sashazaicev/src/UI/Container.js
+++ src/team/sashazaicev/src/UI/Container.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PComponents from "./PComponent";
+import {useSelector} from "react-redux";
 
-const Container = () => {
+const Container = (props) => {
+const main = useSelector((state)=> state.main);
     return (
         <div>
-            <PComponents/>
+            <PComponents textValue={main.textValue} inputValue={main.inputValue} />
         </div>
     )
 
