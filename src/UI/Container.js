0065d07dc46dc92c9904bf4314c58c45861e453e
Ignat <ai73a@yandex.ru>
2019-10-19 17:26:25 +0300

    xxx

---
+++ src/UI/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Container() {
+    return (
+        <div>
+            <div>Text</div>
+            <div><input/></div>
+            <div><button>Send</button></div>
+        </div>
+    );
+}
+
+export default Container;
