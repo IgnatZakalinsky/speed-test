67dda422b87f090bc2361d6a94bf18b8d94ac1e5
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2019-10-19 19:30:29 +0300

    create AC and useDispatch, add onChange and onClick

--- src/team/Oleg/test/src/UI/Container.js
+++ src/team/Oleg/test/src/UI/Container.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PComponent from "./PComponent";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {setInputValueAC, setTextValueAC} from "../BLL/reducer";
 
 function Container() {
     const main = useSelector(state=>state.main)
+    const dispatch = useDispatch()
+    const setInputValue = (value) => {
+        dispatch(setInputValueAC(value))
+    }
+    const setTextValue = () => {
+        dispatch(setTextValueAC())
+    }
     return (
-        <PComponent textValue={main.textValue} inputValue={main.inputValue}/>
+        <PComponent textValue={main.textValue}
+                    inputValue={main.inputValue} setInputValue={setInputValue} setTextValue={setTextValue}/>
     );
-
 }
 
 export default Container
